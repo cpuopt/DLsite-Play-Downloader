80bc1334d5cf146da0843125909150e0527a6c90
Berwyn Jamesson <berwyn.codeweaver@gmail.com>
2026-06-15 21:40:54 -0400

    Use Viewer API instead of V3

--- DLsite-Play-Downloader-vue/src/dlsiteplay-enc-detector/xor.js
+++ DLsite-Play-Downloader-vue/src/dlsiteplay-enc-detector/xor.js
@@ -163,7 +163,7 @@ const hookApiV2Work = () => {
 
     const patterns = [
       /\/api\/v2\/work\/.+$/,
-      /\/api\/v3\/viewer\/token\/.+$/,
+      /\/api\/viewer\/work\/.+$/,
       /\/api\/comipo\/v2\/work\/.+$/
     ];
 
